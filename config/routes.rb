3435413f487bb8446f4769b66f86c53c02572ed0
Yaroslav Shmarov <yshmarov@gmail.com>
2024-08-11 14:03:05 +0200

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static/index'
   get 'static/dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "static#index"
 end
